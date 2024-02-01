f056e3ec3f208ed6ec1ddf57732a9722fafe4558
Metrinenaliaka <metrinenam@gmail.com>
2024-02-01 04:32:33 -0800

    fixed errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-'''fabfile tar packaging'''
-from fabric.api import local, run, prefix, env
+"""
+    script that generates a .tgz archive,
+    from the contents of the web_static
+"""
+from fabric.api import local
 from datetime import datetime
-import os
-
-env.hosts = ['localhost']
+import os.path
 
 
 def do_pack():
-    n = datetime.now()
-    name = "web_static_{}{}{}{}{}{}.tgz".format(n.year, n.month,
-                                                n.day, n.hour,
-                                                n.minute, n.second)
-    local('mkdir -p versions')
-    local("tar -cvzf versions/{} web_static".format(name))
-    size = os.stat("versions/{}".format(name)).st_size
-    print("web_static packed: versions/{} -> {}".format(name, size))
+    """generationg a tgz file"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date)
+    if os.path.isdir("versions") is False:
+        local(" mkdir versions")
+    local('tar -cvzf ' + file_path + ' web_static')
+    if os.path.exists(file_path):
+        return file_path
+    return None
