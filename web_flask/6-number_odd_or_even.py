2e89cce9fa95e1dd7dca9d71defbeb2a5be0ad00
Metrinenaliaka <metrinenam@gmail.com>
2024-02-16 00:11:30 -0800

    dynamic templates if/else/for

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
 '''crates a flask web application'''
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
+app.url_map.strict_slashes=False
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 @app.route('/')
 def hello():
-    """routing home"""
+    """routing for home"""
     return('Hello HBNB!')
-
 
 @app.route('/hbnb')
 def hbnb():
     """routing with path"""
     return('HBNB')
 
-
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """rendering string"""
+    """string rendering"""
     return('C %s' % text.replace("_", " "))
 
-
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python', defaults={'text':'is cool'})
 @app.route('/python/<text>')
 def python_is_cool(text):
-    """rendering string with template"""
+    """string rendering"""
     return('Python %s' % text.replace("_", " "))
-
 
 @app.route('/number/<int:n>')
 def num(n):
-    """rendering int w/o template"""
+    """integer rendering"""
     return('%d is a number' % n)
-
 
 @app.route('/number_template/<int:n>')
 def num_tem(n):
-    """rendering int with template"""
+    """integer rendering"""
     return render_template('5-number.html', n=n)
 
+@app.route('/number_odd_or_even/<int:n>')
+def num_temp(n):
+    """odd or even"""
+    return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
